ab0eace3e6263f96570a0865e4c6a01872edf551
Eric Angle <angle@Erics-MacBook-Pro.local>
2013-08-14 18:48:47 -0600

    Test had same issue with evolvedPop linked to pop.wq

--- gameOfLifeTest.py
+++ gameOfLifeTest.py
@@ -3,8 +3,8 @@ import evolve
 pop = [[0 for x in xrange(2)] for y in xrange(2)]
 pop[1][0] = 1
 pop[0][1] = 1
-popAccept = pop
-popAccept[0][0] = 1
+pop[1][1] = 1
+popAccept = [[1 for x in xrange(2)] for y in xrange(2)]
 popEvolved = evolve.evolve(pop, 2, 2, 1, 0)
 if (popEvolved == popAccept):
   print 'SUCCESS'
